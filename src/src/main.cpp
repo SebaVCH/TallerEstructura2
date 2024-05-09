8aa9a39d223cb19cca15769f8b217f3b70d03ba0
SebaVCH <109924179+SebaVCH@users.noreply.github.com>
2024-05-09 15:26:05 -0400

    Atencion y agregacion de clientes

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -5,36 +5,92 @@
 #include "../include/clienteGeneral.h"
 #include "../include/clienteNormal.h"
 #include "../include/clientePreferencial.h"
-//prueba1
 
 using namespace std;
 
-
-void atenderCliente();
-void cargarProductosBodega();
-void guardarDatosBodega();
 queue<clienteGeneral*> cargarDatosClientesOrdenados();
-void guardarDatosClientes();
+void agregarCliente(queue<clienteGeneral*> &lista);
+queue<clienteGeneral *> ordenarSegunPreferencia(queue<clienteGeneral *> &lista);
+void llamarSiguienteCliente(queue<clienteGeneral*> &lista);
 
+void menu(queue<clienteGeneral *> queue1);
 
 int main() {
 
     queue<clienteGeneral*> listaClientes = cargarDatosClientesOrdenados();
-
-    
-    while (!listaClientes.empty()){
-        cout << listaClientes.front()->getNombre() << endl;
-        listaClientes.pop();
-
-    }
+    menu(listaClientes);
 
     return 0;
+}
+
+void menu(queue<clienteGeneral *> listaDeClientes) {
+
+    int opcion;
+    do {
+        cout << "Seleccione una opción:" << endl;
+        cout << "1. Agregar cliente" << endl;
+        cout << "2. Llamar siguiente cliente" << endl;
+        cout << "0. Salir" << endl;
+        cout << "Opción: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                agregarCliente(listaDeClientes);
+                break;
+            case 2:
+                llamarSiguienteCliente(listaDeClientes);
+                break;
+            case 0:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opción no válida. Inténtelo de nuevo." << endl;
+        }
+    } while (opcion != 0);
+}
+
+void agregarCliente(queue<clienteGeneral*> &lista) {
+
+    //Agregar cliente a la fila
+    string nombre;
+    string tipoCliente;
+    cout << "Ingrese el nombre del cliente: ";
+    cin >> nombre;
+
+    cout << "Ingrese el tipo de cliente (Tercera edad, Discapacidad, Embarazada, Normal): ";
+    cin >> tipoCliente;
+    if (tipoCliente == "Tercera edad" || tipoCliente == "Discapacidad" || tipoCliente == "Embarazada" || tipoCliente == "Normal") {
+        if (tipoCliente == "Normal") {
+            lista.push(new clienteNormal(nombre));
+        } else {
+            lista.push(new clientePreferencial(nombre, tipoCliente));
+        }
+        cout << "Cliente agregado correctamente a la cola." << endl;
+    } else {
+        cout << "Tipo de cliente inválido. Inténtelo de nuevo." << endl;
+    }
+
+    //Ordenar para mantener la preferencia
+    ordenarSegunPreferencia(lista);
+
+}
+
+
+void llamarSiguienteCliente(queue<clienteGeneral*> &lista) {
+    //Llamar al siguiente cliente
+    if (!lista.empty()) {
+        cout << "Cliente llamado: " << lista.front()->getNombre() << endl;
+        delete lista.front();
+        lista.pop();
+    } else {
+        cout << "No hay clientes en espera." << endl;
+    }
 }
 
 queue<clienteGeneral*> cargarDatosClientesOrdenados() {
 
     //Cargar datos de los clientes
-
     queue<clienteGeneral*> lista;
     ifstream archivo("D:\\CLionProjects\\TallerEstructura2\\src\\data\\clientes.txt");
     string linea;
@@ -47,7 +103,7 @@ queue<clienteGeneral*> cargarDatosClientesOrdenados() {
 
             getline(ss, nombre, ',');
 
-            if (!ss.eof()) { // Verificar si hay más datos después de la coma
+            if (!ss.eof()) {
                 getline(ss, tipoCliente);
                 lista.push(new clientePreferencial(nombre, tipoCliente));
             } else {
@@ -58,6 +114,13 @@ queue<clienteGeneral*> cargarDatosClientesOrdenados() {
     } else {
         cout << "No se pudo abrir el archivo clientes.txt" << endl;
     }
+
+    //Ordenar luego de agregar los datos
+    return ordenarSegunPreferencia(lista);
+
+}
+
+queue<clienteGeneral *> ordenarSegunPreferencia(queue<clienteGeneral *> &lista) {
     //Ordenar la fila por orden preferencial
     queue<clienteGeneral*> terceraEdad;
     queue<clienteGeneral*> discapacidad;
@@ -103,5 +166,4 @@ queue<clienteGeneral*> cargarDatosClientesOrdenados() {
     }
 
     return ordenada;
-
 }
